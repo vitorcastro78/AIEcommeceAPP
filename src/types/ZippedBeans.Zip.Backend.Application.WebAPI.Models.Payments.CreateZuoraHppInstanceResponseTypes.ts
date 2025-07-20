b0fa22ec9717d2049a0343df3604c4101b3da604
Vitor Castro <vitorcastro78@gmail.com>
2025-07-20 04:47:40 +0100

    Add ZippedBeans.Zip.Backend.Application.WebAPI.Models.Payments.CreateZuoraHppInstanceResponseTypes component

---
+++ src/types/ZippedBeans.Zip.Backend.Application.WebAPI.Models.Payments.CreateZuoraHppInstanceResponseTypes.ts
@@ -0,0 +1,12 @@
+// ZippedBeans.Zip.Backend.Application.WebAPI.Models.Payments.CreateZuoraHppInstanceResponse.ts
+
+export interface CreateZuoraHppInstanceResponse {
+  subscriptionAccountId: string;
+  signature: string;
+  token: string;
+  tenantId: string;
+  key: string;
+  pageId: string;
+  url: string;
+  paymentGateway: string;
+}
